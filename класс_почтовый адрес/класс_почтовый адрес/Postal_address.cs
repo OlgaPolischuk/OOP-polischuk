fa8d9a307cad6ae112a982290ee477753a116522
Ольга Полищук < >
2017-03-07 13:04:39 +0300

    07.03.2017

---
+++ класс_почтовый адрес/класс_почтовый адрес/Postal_address.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace класс_почтовый_адрес
+{
+    class Postal_address
+    {
+        public int index = 430011;
+        public string city="Saransk";
+        public string street = "Proletarskaya";
+        public int nomber_house = 108;
+        public int nomber_korp = 1;
+        public int nomber_kv = 1;
+
+        public void Edit(int choice, string new_edit)
+        {
+            switch (choice)
+            {
+                case 1:
+                    index = Convert.ToInt32(new_edit);
+                    break;
+                case 2:
+                    city = new_edit;
+                    break;
+                case 3:
+                    street = new_edit;
+                    break;
+                case 4:
+                    nomber_house = Convert.ToInt32(new_edit);
+                    break;
+                case 5:
+                    nomber_korp = Convert.ToInt32(new_edit);
+                    break;
+                case 6:
+                    nomber_kv = Convert.ToInt32(new_edit);
+                    break;
+            }
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Индекс: " + index);
+            Console.WriteLine("Город: " + city);
+            Console.WriteLine("Улица: " + street);
+            Console.WriteLine("Номер дома: " + nomber_house);
+            Console.WriteLine("Корпус: " + nomber_korp);
+            Console.WriteLine("Квартира: " + nomber_kv);
+            Console.WriteLine();
+        }           
+    }
+}
---
+++ класс_почтовый адрес/класс_почтовый адрес/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace класс_почтовый_адрес
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch = 5;
+            var obj = new Postal_address();
+
+            while (ch != 0)
+            {
+                Console.WriteLine("1. Изменить адрес.");
+                Console.WriteLine("2. Вывести адрес.");
+                Console.WriteLine("0. Выход.");
+                Console.WriteLine();
+                ch = Convert.ToInt32(Console.ReadLine());
+
+                if (ch == 1)
+                {
+                    int choice;
+                    string new_edit = "";
+
+                    Console.WriteLine("Изменить: ");
+                    Console.WriteLine("1. Индекс.");
+                    Console.WriteLine("2. Город.");
+                    Console.WriteLine("3. Улицу.");
+                    Console.WriteLine("4. Номер дома.");
+                    Console.WriteLine("5. Корпус.");
+                    Console.WriteLine("6. Квартирy.");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Ведите новое значение поля: ");
+                            new_edit = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Ведите новое значение поля: ");
+                            new_edit = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Ведите новое значение поля: ");
+                            new_edit = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Ведите новое значение поля: ");
+                            new_edit = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Ведите новое значение поля: ");
+                            new_edit = Console.ReadLine();
+                            break;
+                        case 6:
+                            Console.WriteLine("Ведите новое значение поля: ");
+                            new_edit = Console.ReadLine();
+                            break;
+                    }
+
+                    obj.Edit(choice, new_edit);
+                    Console.Clear();
+                }
+                else if (ch == 2)
+                {
+                    obj.Print();
+                }
+            }
+        }
+    }
+}
